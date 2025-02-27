3f6c4a3766fa1efbc7457b768225cb411279bdd2
Jonathan Davidson <jonathan.davidson@canterbury.ac.nz>
2025-02-27 14:04:51 +1300

    use uv

--- script/updateGroup.py
+++ script/updateGroup.py
@@ -1,4 +1,3 @@
-import csv
 import requests
 import argparse
 import logging
@@ -24,7 +23,9 @@ def main():
         if len(matching_groups) == 1:
 
             logging.info('Updating Group with default permissions')
-            default_group = next((g for g in results if g['name'] == 'Default'), None)  # just get the first matching group
+            # just get the first matching group
+            default_group = next(
+                (g for g in results if g['name'] == 'Default'), None)
 
             group_data = {
                 "name": args.group_name,
@@ -39,24 +40,31 @@ def main():
                 logging.info(f"Updated group with data: {group_data}")
 
             else:
-                logging.error(f"Failed to create group: {response}, {response.json()}")
+                logging.error(
+                    f"Failed to create group: {response}, {response.json()}")
                 return
 
         else:
-            logging.error(f"Failed to get group as more than 1 group matches: {matching_groups}")
+            logging.error(
+                f"Failed to get group as more than 1 group matches: {matching_groups}")
             return
 
 
 if __name__ == "__main__":
     # Define the URL of your WebODM instance
     # Set up logging
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
 
     # Set up argument parsing
-    parser = argparse.ArgumentParser(description='Create WebODM accounts from a CSV file.')
-    parser.add_argument('--admin_username', required=True, help='The admin username')
-    parser.add_argument('--group_name', required=True, type=str, help='The Name of the group to add the users to')
-    parser.add_argument('--webodm_url', required=True, type=str, help='url to your inbsance of WebODM. eg. "http://132.181.102.65:8000/" ')
+    parser = argparse.ArgumentParser(
+        description='Create WebODM accounts from a CSV file.')
+    parser.add_argument('--admin_username', required=True,
+                        help='The admin username')
+    parser.add_argument('--group_name', required=True, type=str,
+                        help='The Name of the group to add the users to')
+    parser.add_argument('--webodm_url', required=True, type=str,
+                        help='url to your inbsance of WebODM. eg. "http://132.181.102.65:8000/" ')
 
     # Parse the arguments
     args = parser.parse_args()
@@ -78,6 +86,6 @@ if __name__ == "__main__":
         main()
 
     else:
-        logging.error(f"Failed to authenticate")
+        logging.error("Failed to authenticate")
 
     logging.info("Script ended.")
